6fc2f2c69606eda32aac02840e93152db31f2dbf
최영찬 <gks54410@gmail.com>
2022-03-21 14:00:11 +0900

    Initial Commit

---
+++ src/test/java/jpa/queryDsl/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.queryDsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
