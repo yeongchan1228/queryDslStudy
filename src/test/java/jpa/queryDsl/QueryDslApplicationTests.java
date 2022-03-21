bbb54a5b6cb73e0fd609aafc33cd0ce83368e55e
최영찬 <gks54410@gmail.com>
2022-03-21 19:18:28 +0900

    Test: queryDsl 설정 확인을 위한 테스트

--- src/test/java/jpa/queryDsl/QueryDslApplicationTests.java
+++ src/test/java/jpa/queryDsl/QueryDslApplicationTests.java
@@ -1,13 +1,37 @@
 package jpa.queryDsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpa.queryDsl.hello.entity.Hello;
+import jpa.queryDsl.hello.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+	@Autowired  EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query.selectFrom(qHello).fetchOne();
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(1);
+
+
 	}
 
 }
