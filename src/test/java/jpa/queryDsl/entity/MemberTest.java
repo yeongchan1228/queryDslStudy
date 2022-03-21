cca224a2b92215124969a6c954238fa7437d5a19
최영찬 <gks54410@gmail.com>
2022-03-21 21:19:31 +0900

    Test: queryDsl 예제 도메인 모델 동작 확인 테스트

---
+++ src/test/java/jpa/queryDsl/entity/MemberTest.java
@@ -0,0 +1,53 @@
+package jpa.queryDsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional(readOnly = true)
+class MemberTest {
+
+    @Autowired EntityManager em;
+
+    @Test
+    @Transactional
+    public void testEnitity() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 20, teamA);
+        Member memberC = new Member("memberC", 30, teamB);
+        Member memberD = new Member("memberD", 40, teamB);
+        em.persist(memberA);
+        em.persist(memberB);
+        em.persist(memberC);
+        em.persist(memberD);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+
+        // then
+        for (Member member : result) {
+            System.out.println("member = " + member);
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+    }
+
+}
