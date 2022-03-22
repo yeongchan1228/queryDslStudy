94b6fb637dc7d4cba8c9095568e3c5dda9eb3aaf
최영찬 <gks54410@gmail.com>
2022-03-22 20:57:43 +0900

    Feat: @QueryProjection 추가

--- src/main/java/jpa/queryDsl/dto/MemberDto.java
+++ src/main/java/jpa/queryDsl/dto/MemberDto.java
@@ -1,14 +1,20 @@
 package jpa.queryDsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class MemberDto {
 
     private String username;
     private int age;
+
+    @QueryProjection // 작성 후 Gradle -> other -> compileQuerydsl : Dto를 QDto로
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 }
