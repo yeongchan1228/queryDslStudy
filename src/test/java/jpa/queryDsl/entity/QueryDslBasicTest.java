0bfacb797a19bd7606c1ceee1954254a23413761
최영찬 <gks54410@gmail.com>
2022-03-22 16:06:58 +0900

    Test: on절 join 테스트

--- src/test/java/jpa/queryDsl/entity/QueryDslBasicTest.java
+++ src/test/java/jpa/queryDsl/entity/QueryDslBasicTest.java
@@ -269,4 +269,43 @@ public class QueryDslBasicTest {
         assertThat(result.size()).isEqualTo(2);
     }
 
+    /**
+     * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만, 회원은 모두 조회
+     * JPQL : select t, m from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filter() throws Exception {
+
+        List<Tuple> result = query
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void join_on_no_relation() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        /**
+         * join(대상, 조인할 값) -> join(대상).on(조인할 값)
+         */
+        List<Tuple> result = query
+                .select(member, team)
+                .from(member)
+                .join(team).on(team.name.eq(member.username))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
 }
