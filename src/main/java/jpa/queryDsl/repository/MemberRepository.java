72f586565eb5bce31ca417fe43a3d3c87b6a5620
최영찬 <gks54410@gmail.com>
2022-03-23 17:21:50 +0900

    Feat: 스프링 데이터 JPA 회원 리포지토리 추가

---
+++ src/main/java/jpa/queryDsl/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package jpa.queryDsl.repository;
+
+import jpa.queryDsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
+}
