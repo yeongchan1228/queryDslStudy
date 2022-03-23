cb811819537000c04ba30f117c18ea1dbbcba4a8
최영찬 <gks54410@gmail.com>
2022-03-23 18:47:02 +0900

    Feat: Custom 리포지토리 상속

--- src/main/java/jpa/queryDsl/repository/MemberRepository.java
+++ src/main/java/jpa/queryDsl/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     List<Member> findByUsername(String username);
 }
