e899c26c4b37e9752b43a3a2b932d66cbd3273c2
최영찬 <gks54410@gmail.com>
2022-03-23 19:21:23 +0900

    Feat: QuerydslPredicateExecutor 추가

--- src/main/java/jpa/queryDsl/repository/MemberRepository.java
+++ src/main/java/jpa/queryDsl/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package jpa.queryDsl.repository;
 
 import jpa.queryDsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);
 }
