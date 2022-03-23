dad07c8dfef4771f4d33eb0364548348bb90b9a8
최영찬 <gks54410@gmail.com>
2022-03-23 15:49:49 +0900

    Test: 동적 쿼리 Builder 테스트

--- src/test/java/jpa/queryDsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/jpa/queryDsl/repository/MemberJpaRepositoryTest.java
@@ -1,10 +1,14 @@
 package jpa.queryDsl.repository;
 
+import jpa.queryDsl.dto.MemberTeamDto;
+import jpa.queryDsl.dto.SearchCond;
 import jpa.queryDsl.entity.Member;
+import jpa.queryDsl.entity.Team;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -15,6 +19,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(value = false)
 class MemberJpaRepositoryTest {
 
     @Autowired EntityManager em;
@@ -51,5 +56,32 @@ class MemberJpaRepositoryTest {
         // then
         assertThat(all).containsExactly(member1);
         assertThat(all2).containsExactly(member1);
+    }
+
+    @Test
+    public void builderTest() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 20, teamA);
+        Member memberC = new Member("memberC", 30, teamB);
+        Member memberD = new Member("memberD", 40, teamB);
+        em.persist(memberA);
+        em.persist(memberB);
+        em.persist(memberC);
+        em.persist(memberD);
+
+        // when
+        SearchCond searchCond = new SearchCond("memberA", "teamA", null, null);
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(searchCond);
+
+        // then
+        for (MemberTeamDto memberTeamDto : result) {
+            System.out.println("memberTeamDto = " + memberTeamDto);
+        }
     }
 }
